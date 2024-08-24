793695a9e1d4c9f0082b886ad58ef413543a12a5
SATYABRATA DAS <87033616+Satya12325@users.noreply.github.com>
2024-08-24 23:28:57 +0530

    fix button navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { SideNavbar } from './Components/Navbar/Navbar';
 import { Profile } from './Components/Profile/Profile';
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,21 +3,35 @@ import "../../App.css";
 import { navIcons } from "../../Constant/Images/NavIcons";
 
 export const SideNavbar = ({ children }) => {
-  console.log(children)
+  console.log(children);
   console.log(navIcons);
   return (
     <>
       <div className="mainlayout">
-        <div className="sideNav">
-          <img src="../../Images/NavIcons/Home.svg" alt="" />
-          <div className="iconList">
-            {navIcons?.map((item) => (
-              <div className="Nav">
-                <img src={item} alt="" className="navIcons" />
-              </div>
-            ))}
+        <div className="allNav">
+          <div className="sideNav">
+            <div className="iconList">
+              {navIcons?.map((item) => (
+                <div className="Nav">
+                  <img src={item} alt="" className="navIcons" />
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="bottomNavBar">
+          <div className="bottomNav">
+            <div className="bottomIconList">
+              {navIcons?.map((item) => (
+                <div className="btnNav">
+                  <img src={item} alt="" className="navIcons" />
+                </div>
+              ))}
+            </div>
+          </div>
+
           </div>
         </div>
+
         <div>{children}</div>
       </div>
     </>
